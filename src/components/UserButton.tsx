9a9698f191a18212380f2e8536ff0b5aae8eaf07
IndieCoderMM <hthant00chk@gmail.com>
2023-09-15 22:33:23 +0630

    Create profile page

--- src/components/UserButton.tsx
+++ src/components/UserButton.tsx
@@ -2,6 +2,7 @@ import { AuthStatus, avatars } from "../lib/constants";
 
 import { selectAuthStatus, selectUser } from "../features/User/userSlice";
 import { useAppSelector } from "../lib/hooks";
+import { Link } from "react-router-dom";
 
 const UserButton = () => {
   const authStatus = useAppSelector(selectAuthStatus);
@@ -10,14 +11,14 @@ const UserButton = () => {
   if (authStatus !== AuthStatus.SignedIn) return null;
 
   return (
-    <div className="flex items-center">
+    <Link to="/profile" className="flex items-center">
       <img
         src={avatars[currentUser!.avatarId]}
         alt={currentUser!.name}
         className="h-9 w-9 rounded-full"
       />
       <span className="sr-only">{currentUser!.name}</span>
-    </div>
+    </Link>
   );
 };
 
--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,7 +1,63 @@
-import React from "react";
+import { useAppSelector } from "../lib/hooks";
+import { selectUser } from "../features/User/userSlice";
+import { avatars } from "../lib/constants";
 
 const Profile = () => {
-  return <div>Profile</div>;
+  const currentUser = useAppSelector(selectUser);
+
+  return (
+    <section className="p-2">
+      <h1 className="lg:text-5xl mb-4 text-2xl font-bold md:text-3xl">
+        My Account
+      </h1>
+      <div className="w-full max-w-5xl space-y-4 rounded-md bg-dark-800 p-2 lg:rounded-xl lg:p-4">
+        <header className="overflow-hidden rounded-md bg-indigo-500 lg:rounded-xl">
+          <div className="h-40" />
+          <div className="relative flex items-center justify-between bg-dark-700 p-2">
+            <div className="flex items-center">
+              <img
+                src={avatars[currentUser?.avatarId!]}
+                alt=""
+                className="h-24 w-24 rounded-full bg-dark-700 p-2"
+              />
+              <h2 className="text-2xl font-bold">{currentUser?.name}</h2>
+            </div>
+            <button type="button" className="btn">
+              Change Avatar
+            </button>
+          </div>
+        </header>
+        <div className="flex items-center justify-between pt-4">
+          <h3 className="text-xl font-bold">Account Details</h3>
+          <button type="button" className="btn">
+            Edit
+          </button>
+        </div>
+        <main className="rounded-md bg-dark-700 p-4 lg:rounded-xl">
+          <div className="flex items-center justify-between p-1">
+            <div className="flex flex-col gap-1">
+              <h4 className="text-lg font-semibold">Username</h4>
+              <p className="text-sm text-gray-400">{currentUser?.name}</p>
+            </div>
+          </div>
+          <div className="flex items-center justify-between p-1">
+            <div className="flex flex-col gap-1">
+              <h4 className="text-lg font-semibold">Email</h4>
+              <p className="text-sm text-gray-400">{currentUser?.email}</p>
+            </div>
+          </div>
+          <div className="flex items-center justify-between p-1">
+            <div className="flex flex-col gap-1">
+              <h4 className="text-lg font-semibold">Bio</h4>
+              <p className="text-sm text-gray-400">
+                {currentUser?.bio || "No information yet"}
+              </p>
+            </div>
+          </div>
+        </main>
+      </div>
+    </section>
+  );
 };
 
 export default Profile;
