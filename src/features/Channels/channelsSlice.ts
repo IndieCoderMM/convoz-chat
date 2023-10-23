4d1e3ba850a37fca91d68a06ff4a12b1f9fd18aa
IndieCoderMM <hthant00chk@gmail.com>
2023-10-23 22:07:13 +0630

    Memoized selectors for performance

--- src/features/Channels/channelsSlice.ts
+++ src/features/Channels/channelsSlice.ts
@@ -1,4 +1,4 @@
-import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { createSelector, createSlice, PayloadAction } from "@reduxjs/toolkit";
 
 import { ChannelInterface } from "../../common.types";
 import { ChannelsStatus } from "../../lib/constants";
@@ -27,33 +27,46 @@ const channelsSlice = createSlice({
 
 export const { setChannels } = channelsSlice.actions;
 
-export const getAllChannels = (state: RootState) => state.channels.data;
+const getChannels = (state: RootState) => state.channels;
 
-export const getChannelById = (state: RootState, channelId: string) => {
-  return state.channels.data.find((channel) => channel.id === channelId);
-};
+export const getAllChannels = createSelector(getChannels, (channels) => {
+  return channels.data;
+});
 
-export const getStaticChannels = (state: RootState) => {
-  return state.channels.data.filter(
+export const getChannelById = createSelector(
+  [getChannels, (_state, id) => id],
+  (channels, id) => {
+    return channels.data.find((channel) => channel.id === id);
+  },
+);
+
+export const getStaticChannels = createSelector(getChannels, (channels) => {
+  return channels.data.filter(
     (channel) => channel.type === "static" || channel.type === "announcement",
   );
-};
+});
 
-export const getPublicChannels = (state: RootState) => {
-  return state.channels.data.filter((channel) => channel.type === "public");
-};
+export const getPublicChannels = createSelector(getChannels, (channels) => {
+  return channels.data.filter((channel) => channel.type === "public");
+});
 
-export const getJoinedChannels = (state: RootState, userId: string) => {
-  return state.channels.data.filter(
-    (channel) =>
-      channel.type !== "static" &&
-      channel.type !== "announcement" &&
-      channel.members.includes(userId),
-  );
-};
+export const getJoinedChannels = createSelector(
+  [getChannels, (_state, userId) => userId],
+  (channels, userId) => {
+    return channels.data.filter(
+      (channel) =>
+        channel.type !== "static" &&
+        channel.type !== "announcement" &&
+        channel.members.includes(userId),
+    );
+  },
+);
 
-export const getCreatedChannels = (state: RootState, userId: string) => {
-  return state.channels.data.filter((channel) => channel.createdBy === userId);
-};
+export const getCreatedChannels = createSelector(
+  [getChannels, (_state, userId) => userId],
+  (channels, userId) => {
+    return channels.data.filter((channel) => channel.createdBy === userId);
+  },
+);
 
 export default channelsSlice.reducer;
